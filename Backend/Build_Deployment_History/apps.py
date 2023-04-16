3813c1237293cb333370145321a18755d6618f7d
Closeddd <shayne.khaw@gmail.com>
2023-04-16 13:54:48 +0800

    recreating project, deleting first to prevent merge issues

--- Backend/Build_Deployment_History/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BuildDeploymentHistoryConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Build_Deployment_History'
