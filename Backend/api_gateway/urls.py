8150e689db396a78c3b55d020a3a59d87524a8ec
Closeddd <shayne.khaw@gmail.com>
2023-05-20 14:35:11 +0800

    added JWT generation adn homepage view and url

--- Backend/api_gateway/urls.py
+++ Backend/api_gateway/urls.py
@@ -23,5 +23,8 @@ urlpatterns = [
     
     #authentication application endpoints
     path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login')
+    path('login/', views.login_view, name='login'),
+    path('home/',views.home,name='home'),
+    path('logout/',views.logout_view,name='logout')
+
 ]
