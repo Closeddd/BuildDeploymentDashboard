3d027fb6b711f589fa1c109e4e7aa806344b7ed4
Closeddd <shayne.khaw@gmail.com>
2023-07-12 21:21:34 +0800

    updated views.py

--- authentication/views.py
+++ authentication/views.py
@@ -1,14 +1,8 @@
-import bcrypt
-from django.contrib.auth import login, logout
-from django.contrib.auth.models import User
-from django.http import JsonResponse
 from django.shortcuts import redirect
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.utils import json
-from rest_framework_simplejwt.tokens import RefreshToken
 
 from authentication import service
 
