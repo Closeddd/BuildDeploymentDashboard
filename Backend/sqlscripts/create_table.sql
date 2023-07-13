afc349c42592818ac43f4cbc7ad8b945e2222fa5
redbeanbreadhasgit <69857133+redbeanbreadhasgit@users.noreply.github.com>
2023-07-13 21:35:32 +0800

    removing backend folder

--- Backend/sqlscripts/create_table.sql
+++
@@ -1,9 +0,0 @@
-USE builddeploydashboard;
-
-CREATE TABLE IF NOT EXISTS auth_users(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(150) NOT NULL UNIQUE,
-    password VARCHAR(128) NOT NULL ,
-    email VARCHAR(150) NOT NULL UNIQUE,
-    role varchar(50) NULL 
-);
